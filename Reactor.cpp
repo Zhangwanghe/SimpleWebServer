b0dd3204306cddc9bb5d68ce307eae9e38c12e26
Zhangwanghe <13384228948@163.com>
2022-08-12 14:01:49 +0800

    add epollinit

--- Reactor.cpp
+++ Reactor.cpp
@@ -2,15 +2,22 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-
 #include<string.h>
 #include <cassert>
+
+#include <sys/epoll.h>
 
 void Reactor::init(int port) {
     m_port = port;
 }
 
 void Reactor::startup() {
+    init_listen();
+    init_epoll();    
+    add_epoll(m_listenfd);
+}
+
+void Reactor::init_listen() {
     m_listenfd = socket(AF_INET, SOCK_STREAM, 0);
     assert(m_listenfd >= 0);
 
@@ -25,4 +32,16 @@ void Reactor::startup() {
     assert(ret >= 0);
     ret = listen(m_listenfd, 10);
     assert(ret >= 0);
+}
+
+void Reactor::init_epoll() {
+    m_epollfd = epoll_create(0);
+    assert(m_epollfd >= 0);
+}
+
+void Reactor::add_epoll(int fd) {
+    epoll_event event;
+    event.data.fd = fd;
+
+    epoll_ctl(m_epollfd, EPOLL_CTL_ADD, fd, &event);
 }
--- Reactor.h
+++ Reactor.h
@@ -1,12 +1,16 @@
 class Reactor {
     int m_port;
     int m_listenfd;
+    int m_epollfd;
+
+    void init_listen();
+
+    void init_epoll();
+
+    void add_epoll(int fd);
 
 public:
-    /*
-    *  param: ip, port - create socket
-    */
     void init(int port);
-
+    
     void startup();
 };
