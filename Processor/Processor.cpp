38c0968c207e86e74adb3f31a0d5ecbfdeaa7880
Zhangwanghe <13384228948@163.com>
2022-08-14 21:38:34 +0800

    add read http logic

--- Processor/Processor.cpp
+++ Processor/Processor.cpp
@@ -1,4 +1,6 @@
 #include "Processor.h"
+#include <sstream>
+#include <iostream>
 using namespace std;
 
 Processor::Processor(const std::shared_ptr<Buffer>& in, const std::shared_ptr<Buffer>& out) {
@@ -7,5 +9,43 @@ Processor::Processor(const std::shared_ptr<Buffer>& in, const std::shared_ptr<Bu
 }
 
 void Processor::run() {
-    
+    processRead();
 }   
+
+void Processor::processRead() {
+    istringstream in(m_bufferIn->buffer);
+
+    string line;
+    getline(in, line);
+    parseRequestLine(line);
+
+    vector<string> headers;
+    while (true) {
+        string header;
+        getline(in, header);
+        
+        if (header.empty()) {
+            parseRequestHeader(headers);
+            break;
+        } 
+
+        headers.push_back(header);
+    }
+
+    // string body;
+    // getline(in, body);
+    // parseRequestBody(body);
+}
+
+void Processor::parseRequestLine(const string& line) {
+
+}
+
+void Processor::parseRequestHeader(const vector<string>& headers) {
+
+}
+
+void Processor::parseRequestBody(const vector<string>& body) {
+
+}
+
--- Processor/Processor.h
+++ Processor/Processor.h
@@ -3,10 +3,20 @@
 #include "../ThreadPool/Runnable.h"
 #include "../public.h"
 #include <memory>
+#include <string>
+#include <vector>
 
 class Processor : public Runnable{
     std::shared_ptr<Buffer> m_bufferIn;
     std::shared_ptr<Buffer> m_bufferOut;
+
+    void processRead();
+
+    void parseRequestLine(const std::string& line);
+
+    void parseRequestHeader(const std::vector<std::string>& headers);
+
+    void parseRequestBody(const std::vector<std::string>& s);
 
 public:
     Processor(const std::shared_ptr<Buffer>& in, const std::shared_ptr<Buffer>& out);
